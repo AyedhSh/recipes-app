180ab0776801c4305af96888611b13254e23d801
AyedhSh <alshmary_ad@yahoo.com>
2017-04-18 19:37:38 +0300

    start creating assosate recipe and it's ingredients

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -5,19 +5,13 @@ class RecipesController < ApplicationController
 
   def new  #get
     @recipe=Recipe.new()
-    # @ingredients=5.times{Ingredient.new}
     @ingredients = (1..5).to_a.collect{@recipe.ingredients.build}
-  #  i=0
-  #  while(i<5){@ingredients[i]=Ingredient.new
-   #   i+=1}
   end
 
 #If i create Kebab with Meat, Salt, and Onion
 # and You create Ayedh's Kebab with Chicken, Pepper, and lettuce
 
-# its very loud there. can you still hear me?
-
-  def create #POST
+  def create   #POST
     # raise.params.inspect
     raise recipe_params.inspect
     # params[:recipe] = {name: "Kebab", id: 12 , ingredient:[name]}
@@ -25,15 +19,10 @@ class RecipesController < ApplicationController
     # add more ingredient fields to the new form
     # create a new recipe and associate ingredients
     # we want to be able to create a recipe with 0-5 ingredients
-    @recipe=Recipe.create(params[:recipe])
-    if @recipe
-      @ingredient = Ingredient.all.find {|ingredient| ingredient.name == params[:ingredient_names][:name] }
-      if !@ingredient
-        render 'show'
-      else 
-        redirect_to 'recipe/new'
-      end
-    end
+    @recipe=Recipe.create(recipe_params)
+     
+        redirect_to recipe_params_path
+      
   end
 
   def show
